b22352846e79d9ff3de5c684515a387de071f373
goce <goce@debian.laravel.local>
2019-09-06 10:08:29 -0400

    Clean ProjectController wo Comments

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -9,42 +9,24 @@ class ProjectController extends Controller
 {
     public function index(Project $project)
     {
-
         $projects = $project->all();
 
-        // return $projects;
-
-        // return view('projects.index', ['projects' => $projects] );
         return view('projects.index', compact('projects'));
     }
 
 
     public function show(Project $project)
     {
-        //$project = Project::findOrFail($id);
-
-        //return $project;
-
         return view('projects.show', compact('project'));
     }
 
     public function edit(Project $project)
     {
-
-
-        //$project = Project::find($id);
-        //$project = Project::findOrFail($id);
-
-
         return view('projects.edit',compact('project'));
     }
 
     public function destroy(Project $project)
     {
-        //dd('Hello Destroy' . $id);
-
-        //Project::find($id)->delete();
-
         $project->delete();
 
         return redirect('/projects');
@@ -52,16 +34,7 @@ class ProjectController extends Controller
 
     public function update(Project $project)
     {
-        //dd(request()->all());
-
-        //$project = Project::find($id);
-
         $project->update(request(['title','description']))
-
-        //$project->title = request('title');
-        //$project->description = request('description');
-
-        //$project->save();
 
         return redirect('/projects');
     }
@@ -73,21 +46,7 @@ class ProjectController extends Controller
 
     public function store()
     {
-
-
         Project::create(request(['title','description']));
-/*
-         $project = new Project();
-
-        $project->title = request('title');
-        $project->description = request('description');
-
-        $project->save();
- */
-
-
-        //return request()->all();
-        //return request('title');
 
         return redirect('/projects');
     }
