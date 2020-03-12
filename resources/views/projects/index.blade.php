52e97cf176f73e1c7d77515c0712e0749013e6c5
Goce Stojanov <gocestojanov@gmail.com>
2020-03-12 16:06:00 -0400

    List Projects Box Styling

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,14 +6,34 @@
 @section('content')
 
     @component('projects.verticalmenu')
-            <h1>Projects</h1>
+
+            <h1 class="title">Browse Projects</h1>
+
+            @foreach ($projects as $project)
+            <div class="box">
+                <article class="media">
+                  <div class="media-left">
+                    <figure class="image is-64x64">
+                      <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <div class="content">
+                      <p>
+                        <strong><a href="/projects/{{$project->id}}">{{ $project->title }}</a> </strong> <small>@johnsmith</small> <small>31m</small>
+                        <br>
+                        {{ $project->description }}
+                      </p>
+                    </div>
+                  </div>
+                </article>
+            </div>
+            @endforeach
 
 
-            <ul>
-                @foreach ($projects as $project)
-                <li><a href="/projects/{{$project->id}}">{{ $project->title }}</a> {{ $project->description }}</li>
-                @endforeach
-            </ul>
+
+
+
 
 
             @if ( session('message'))
