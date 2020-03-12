b906595e4eea0d53defa5c2cfb422e97a041d5dc
Goce Stojanov <gocestojanov@gmail.com>
2020-03-12 16:08:14 -0400

    About Page with 3 columns styling and insert Text

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,22 +1,37 @@
 @extends('layouts.app')
 
-@section('title','About Us')
+@section('title','About')
 
 
 
 
 @section('content')
 
+    <div class="content">
+        <div class="columns">
+        <div class="column is-one-fifth"></div>
+        <div class="column">
+            <h1 class="title">About</h1>
+            <p> <strong>Project VS Tasks</strong> Web Application is made for a testing purpose with Laravel PHP Framework & Bulma, open source CSS framework</p>
+
+            <h1 class="subtitle is-4">Everything that is used in this Application</h1>
+            <h2 class="subtitle is-4">Laravel</h2>
+            <ul>
+                <li> <strong>Localization:</strong>  Path: resources/lang. Retrieving Translation Strings Example:  __('auth.Register'), where auth is file in lang folder. </li>
+                <li> <strong>Blade: Components & Slots</strong>   - verticalmenu after login is component { $slot }</li>
+            </ul>
 
 
 
-    <h1>About US</h1>
+            <h2 class="subtitle is-4">Frontend</h2>
+            <ul>
+                <li> <strong>Bulma CSS Framework:</strong>  <a href="https://bulma.io/" target="_blank">https://bulma.io/</a>  Included into header section.</li>
+                <li> <strong>Fontawesome icons:</strong>  Include into header section. Using: <i class="fas fa-user"></i></li>
+            </ul>
 
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-        necessitatibus maxime! Nesciunt aspernatur repellat velit consequuntur
-        saepe et, nobis nam sit numquam corporis illo facere explicabo,
-        facilis temporibus beatae ratione?</p>
-
+        </div>
+        <div class="column is-one-fifth"></div>
+        </div>
+    </div>
 
 @endsection
