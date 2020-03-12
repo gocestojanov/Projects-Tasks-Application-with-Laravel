cf19c29cc477287e49b577b03c7f1e00194ab3b3
Goce Stojanov <gocestojanov@gmail.com>
2020-03-12 17:16:59 -0400

    Styling Contact Page and Adding Contact Text

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,5 +6,36 @@
 
 
 @section('content')
-    <p>Contact Us</p>
+
+<div class="content">
+    <div class="columns">
+    <div class="column is-one-fifth"></div>
+    <div class="column">
+
+
+        <h2 class="subtitle">Goce Stojanov</h2>
+        <h4 class="subtitle">Laravel Developer</h4>
+
+        <div class="box">
+            <article class="media">
+                <div class="media-left">
+                    <figure class="image is-128x128">
+                      <img src="images/goce.jpg" alt=">Goce Stojanov Picture">
+                    </figure>
+                </div>
+                <div class="media-content">
+                    <strong>Marital status and family:</strong>  Married, 2 children <br>
+                    <strong>Place and date of birth:</strong>  Skopje, R. Macedonia, 11.09.1977 <br>
+                    <strong>Contact:</strong>  Marstallstraße 47D, 68723 Schwetzingen Deutschland, <br>
+                    <strong>Mob:</strong>  +49 152 57114464 <br>
+                    <strong>Email:</strong> gocestojanov@gmail.com <br>
+                </div>
+            </article>
+        </div>
+
+    </div>
+    <div class="column is-one-fifth"></div>
+    </div>
+</div>
+
 @endsection
