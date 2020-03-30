ee950e681eaeb4c8757a945a639c60c63f3f7eeb
Goce Stojanov <gocestojanov@gmail.com>
2020-03-30 15:40:26 -0400

    test 1

--- resources/views/projects/verticalmenu.blade.php
+++ resources/views/projects/verticalmenu.blade.php
@@ -11,7 +11,7 @@
                     <li><a id="link3" class="{{ request('active3') ? 'is-active' : '' }}" href="/projects/create?active3=yes" >Create Project</a></li>
                     </ul>
                     <p class="menu-label">
-                    Administration
+                    Administration 1
                     </p>
                     {{-- <ul class="menu-list">
                     <li><a>Team Settings</a></li>
