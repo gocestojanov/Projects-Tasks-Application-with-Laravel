63bdcccde27a8daf0ab7e0d8dcfda39e54c0913a
Goce Stojanov <gocestojanov@gmail.com>
2020-05-18 17:04:08 -0400

    Welcome Page 3 Images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,15 +7,51 @@
 
 @section('content')
 
-        <h1>Hero we go again!!!</h1>
+    <div class="content">
+        <div class="columns">
+        <div class="column is-one-fifth"></div>
+        <div class="column">
 
-        <ul>
-        @foreach ($tasks as $task)
-            <li>{{ $task }}</li>
-        @endforeach
-        </ul>
+            <section class="hero is-success is-fullheight">
+                <div class="hero-body">
+                  <div class="container">
+                    <h1 class="title">
+                      Projects & Tasks
+                    </h1>
+                    <h2 class="subtitle">
+                      Easy create Unlimited Projects with Unlimited Tasks
+                    </h2>
 
-        {{ $foo }}
+                    <div class="columns">
+                        <div class="column" >
+                            <figure class="image is-256x256">
+                                <img class="is-rounded" src="images/tasks.jpg">
+                            </figure>
+                        </div>
+                        <div class="column" style="align-self: baseline;">
+                            <figure class="image is-128x128">
+                                <img class="is-rounded" src="images/task1.jpg">
+                            </figure>
+                        </div>
+                        <div class="column">
+                            <figure class="image is-64x64">
+                                <img class="is-rounded" src="images/task2.jpg">
+                            </figure>
+                        </div>
+                      </div>
+
+
+
+
+                  </div>
+                </div>
+              </section>
+
+        </div>
+        <div class="column is-one-fifth"></div>
+        </div>
+    </div>
+
 
 @endsection
 
