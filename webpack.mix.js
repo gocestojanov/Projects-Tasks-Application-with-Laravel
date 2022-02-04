79c366362c0d462720ebac95ade5a8db53a9fb94
Goce Stojanov <gocestojanov@gmail.com>
2022-02-04 10:32:27 -0500

    Fix npm packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css').version();
+mix.js('resources/js/app.js', 'public/js').vue()
+.sass('resources/sass/app.scss', 'public/css').version();
+
+// mix.js('resources/js/app.js', 'public/js').vue()
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
+// mix.js('resources/js/app.js', 'public/js').vue();
